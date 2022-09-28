b2d1758ff27bf8e0b239290382d1da6caccbf26a
tituskiprono <114140358+tituskiprono@users.noreply.github.com>
2022-09-28 14:15:33 +0300

    netbonus.cpp

---
+++ netbonus.cpp
@@ -0,0 +1,28 @@
+#include<cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int salary,years, bonus;
+    cout<<"enter salary"<<endl;
+    cin>>salary;
+    cout<<"enter years"<<endl;
+    cin>>years;
+    if(years>10)
+    {
+        bonus=salary*0.1;
+        cout<<"net bonus ="<<bonus <<endl;
+        cin>>bonus;
+           }
+          else if((years>=6)&&(years<=10))
+          {
+              bonus=salary*0.08;
+              cout<<"net bonus="<<bonus<<endl;           cin>>bonus;
+          }
+          else if (years<6)
+          {
+              bonus= salary*0.05;
+              cout<<"net bonus="<<bonus<<endl;         cin>>bonus;
+          }
+          return 0;
+}

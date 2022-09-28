41f76ddd96c184d51c59691d347d3d0bdbda6fa7
tituskiprono <114140358+tituskiprono@users.noreply.github.com>
2022-09-28 14:14:49 +0300

    voting program.cpp

---
+++ voting program.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<"enter your age"<<endl;
+    cin>>age;
+    if (age>=18){
+    cout<<"you are eligible to vote"<<endl;
+    }
+    else if (age<18){
+        cout<<"you are not eligible to vote"<<endl;
+    }
+    
+
+return 0;
+}

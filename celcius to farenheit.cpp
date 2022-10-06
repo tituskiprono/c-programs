85112da1e5d0f7bc0b2b7f307207f54e82efc838
tituskiprono <114140358+tituskiprono@users.noreply.github.com>
2022-10-06 12:29:36 +0300

    Update celcius to farenheit.cpp

--- celcius to farenheit.cpp
+++ celcius to farenheit.cpp
@@ -1,17 +1,17 @@
 //program to convert celcius to farenheit
 #include<iostream>
 using namespace std;
-float faren(float faren);
+float faren(float faren); //function prototype
 int main()
 {
 int f;
 cout<<"Enter temperature in celcius :";
 cin>>f;
-float c=0.5555*(f-32);
+float c=0.5555*(f-32);  //function calling
 cout<<"The temperature in farenheit is :"<<c;
 return 0;	
 }
-float faren(float faren)
+float faren(float faren)  //function definition
 {
 	int f;
 	float c=0.5555*(f-32);

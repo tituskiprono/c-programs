fb3c2009baeb1a47154b7cb7c27b838fdf3d650d
tituskiprono <114140358+tituskiprono@users.noreply.github.com>
2022-10-06 12:12:00 +0300

    Add files via upload

---
+++ celcius to farenheit.cpp
@@ -0,0 +1,19 @@
+//program to convert celcius to farenheit
+#include<iostream>
+using namespace std;
+float faren(float faren);
+int main()
+{
+int f;
+cout<<"Enter temperature in celcius :";
+cin>>f;
+float c=0.5555*(f-32);
+cout<<"The temperature in farenheit is :"<<c;
+return 0;	
+}
+float faren(float faren)
+{
+	int f;
+	float c=0.5555*(f-32);
+	return faren;
+}

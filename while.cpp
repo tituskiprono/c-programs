9a97d8e7290e7ab085dfa685d50046302ecc71af
tituskiprono <114140358+tituskiprono@users.noreply.github.com>
2022-11-15 21:16:32 -1200

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,22 @@
+//promting user to add numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,sum;
+sum=0;
+cout<<"Enter first number";
+ cin>>a;
+ cout<<"Enter last number";
+ cin>>b;
+ while(a<=b)
+ 
+ { 
+ cout<<a <<"\n";
+ sum+=a;
+ a++;
+ 
+ }
+cout<<sum;
+ 
+}
